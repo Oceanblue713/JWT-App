fe60b689c067bf3f2370991a7633f20070ad8f69
Anri Liy <amedamachip713@gmail.com>
2020-06-17 16:24:49 -0700

    Add database

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express()
+const cors = require("cors");
+
+// middleware
+app.use(express.json()) // allow to use req.body
+app.use(cors())
+
+
+//ROUTES
+
 
 app.listen(5000, () => {
   console.log("Server is starting")
